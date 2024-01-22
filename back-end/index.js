b9b8da24cfcd1f808109fefff87d87ac280d6db9
Calisto-Mathias <calisto.mathias.25@gmail.com>
2024-01-22 15:28:43 +0530

    Added Back End
    Created the dbSaver() function in kandi.js

--- back-end/index.js
+++ back-end/index.js
@@ -7,8 +7,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { fileURLToPath } from "url";
 import path from "path";
+import bodyParser from "body-parser";
 
-import { addScore } from "./controllers/addScore";
+import updateController from "./controllers/addScore.js";
+import retrieveController from "./controllers/retrieveScores.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,6 +28,7 @@ app.use(morgan("common"));
 app.use(cookieParser());
 
 app.use("/update", updateController);
+app.use("/retrieve", retrieveController);
 
 const port = process.env.PORT || 3001;
 mongoose
